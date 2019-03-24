288edccec6faed686a5ecacf907811c43446a4cf
Kosam Omollo <hnkhosamomollo3@gmail.com>
2019-03-24 21:22:38 +0300

    Initial commit

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import TextInputGroup from '../layout/TextInputGroup';
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { addContact } from '../../actions/contactAction';
+
+
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    errors: {}
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, phone, address } = this.state;
+
+    // Check For Errors
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is required' } });
+      return;
+    }
+
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is required' } });
+      return;
+    }
+
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' } });
+      return;
+    }
+
+    if(address === '') {
+      this.setState({errors: {address: 'Address is required '}});
+      return;
+    }
+
+    const newContact = {
+      name,
+      email,
+      phone,
+      address
+    };
+
+    this.props.addContact(newContact);
+
+    // Clear State
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+      errors: {}
+    });
+
+    this.props.history.push('/');
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, phone, address, errors } = this.state;
+
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <TextInputGroup
+              label="Name"
+              name="name"
+              placeholder="Enter Name"
+              value={name}
+              onChange={this.onChange}
+              error={errors.name}
+            />
+            <TextInputGroup
+              label="Email"
+              name="email"
+              type="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={this.onChange}
+              error={errors.email}
+            />
+            <TextInputGroup
+              label="Phone"
+              name="phone"
+              placeholder="Enter Phone"
+              value={phone}
+              onChange={this.onChange}
+              error={errors.phone}
+            />
+            <TextInputGroup
+              label="Address"
+              name="address"
+              placeholder="Enter Address"
+              value={address}
+              onChange={this.onChange}
+              error={errors.address}
+            />
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+AddContact.propTypes = {
+  addContact: PropTypes.func.isRequired
+}
+
+export default connect(null, {addContact})(AddContact);
